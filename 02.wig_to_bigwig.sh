7c62146cc77a41fa96e1b1c06e14a1ac99c3f782
Mikhail Nazareth <mikhailnazareth@Mikhails-MacBook-Pro.local>
2026-05-12 14:15:58 +1000

    making changes to the converter to account for Tajima's D - Bigiwig incompatabilities

--- 02.wig_to_bigwig.sh
+++ 02.wig_to_bigwig.sh
@@ -10,5 +10,5 @@ WigToBigWig wig_files/digi_per_snp_pi.wig ./inputs/chrom.sizes BigWig_files/digi
 WigToBigWig wig_files/fat_digi_per_snp_pi.wig ./inputs/chrom.sizes BigWig_files/fat_digi_per_snp_pi.bw
 WigToBigWig wig_files/digi_100bp_window_pi.wig ./inputs/chrom.sizes BigWig_files/digi_100bp_window_pi.bw
 WigToBigWig wig_files/fat_digi_100bp_window_pi.wig ./inputs/chrom.sizes BigWig_files/fat_digi_100bp_window_pi.bw
-WigToBigWig wig_files/fat_digi_tajimas_d_500bp_window.wig ./inputs/chrom.sizes BigWig_files/fat_digi_tajimas_d_500bp_window.bw
-WigToBigWig wig_files/digi_tajimas_d_500bp_window.wig ./inputs/chrom.sizes BigWig_files/digi_tajimas_d_500bp_window.bw
+WigToBigWig wig_files/fat_digi_tajimas_d_500bp_window.wig ./inputs/chrom.sizes BigWig_files/fat_digi_tajimas_d_100bp_window.bw
+WigToBigWig wig_files/digi_tajimas_d_500bp_window.wig ./inputs/chrom.sizes BigWig_files/digi_tajimas_d_100bp_window.bw
--- file_to_wig.py
+++ file_to_wig.py
@@ -10,6 +10,7 @@ import subprocess
 from operator import itemgetter
 from collections import defaultdict
 
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 log = logging.getLogger()
 parser = argparse.ArgumentParser()
 parser.add_argument("--input", required=True, help="Path to Input file")
@@ -25,7 +26,6 @@ Type of file, file can be:
 
 
 """)
-parser.add_argument("--window_step_size", type=int, required=False, help="step size for windowed files")
 
 args = parser.parse_args()
 
@@ -34,7 +34,6 @@ class convert_file:
         self.input = args.input
         self.output = args.output
         self.type = args.type
-        self.window_step_size = args.window_step_size
 
     def write_wig(self, chromsomes):
         with open(self.output, 'w') as f:
@@ -43,16 +42,17 @@ class convert_file:
                    f.write(f"variableStep chrom={chrom}\n")
                    for pos, value in chromsomes[chrom]:
                        f.write(f"{pos}\t{value}\n")
+            ##this function is a repeat of the above but was built to figure out how to impliment fixedStep, which is not reading into IGV
             elif self.type =="window_fst" or self.type == "window_pi" or self.type == "tajimas_d":
                 for chrom in chromsomes:
-                    f.write(f"fixedStep chrom={chrom} start=1 step={self.window_step_size} span={self.window_step_size - 1}\n")
-                    for value in chromsomes[chrom]:
-                        f.write(f"{value}\n")
+                    f.write(f"variableStep chrom={chrom}\n")
+                    for pos, value in chromsomes[chrom]:
+                        f.write(f"{pos}\t{value}\n")
             else:
                 print("Failure at write step. This file type is not supported yet.")
 
     def convert_fst_to_wig(self):
-        log.info(f"Converting FST to WIG for {self.input}")
+        log.info(f"Converting FST to WIG for {self.input}\n")
         chromsomes=defaultdict(list)
         with open(self.input, 'r') as f:
             next(f)
@@ -66,7 +66,7 @@ class convert_file:
         self.write_wig(chromsomes)
 
     def convert_pi_to_wig(self):
-        log.info(f"Converting Pi to WIG for {self.input}")
+        log.info(f"Converting Pi to WIG for {self.input}\n")
         chromsomes=defaultdict(list)
         with open(self.input, 'r') as f:
             next(f)
@@ -80,7 +80,7 @@ class convert_file:
         self.write_wig(chromsomes)
 
     def convert_win_fst_to_wig(self):
-        log.info(f"Converting Windowed FST to WIG for {self.input}")
+        log.info(f"Converting Windowed FST to WIG for {self.input}\n")
         chromsomes=defaultdict(list)
         with open(self.input, 'r') as f:
             next(f)
@@ -90,11 +90,11 @@ class convert_file:
                 start=int(line[1])
                 fst= float(line[4])
                 if fst <0 or math.isnan(fst): fst=0
-                chromsomes[chrom].append(fst)
+                chromsomes[chrom].append((start,fst))
         self.write_wig(chromsomes)
 
     def convert_windowed_pi_to_wig(self):
-        log.info(f"Converting Windowed Pi to WIG for {self.input}")
+        log.info(f"Converting Windowed Pi to WIG for {self.input}\n")
         chromsomes=defaultdict(list)
         with open(self.input,'r') as f:
             next(f)
@@ -104,21 +104,23 @@ class convert_file:
                 start=int(line[1])
                 pi= float(line[4])
                 if pi <0 or math.isnan(pi): pi=0
-                chromsomes[chrom].append(pi)
+                chromsomes[chrom].append((start,pi))
         self.write_wig(chromsomes)
 
     def convert_tajimas_d_to_wig(self):
-        log.info(f"Converting Tajimas D to WIG for {self.input}")
+        log.info(f"Converting Tajimas D to WIG for {self.input}\n")
         chromsomes=defaultdict(list)
         with open(self.input,'r') as f:
             next(f)
             for line in f:
                 line=line.strip().split()
+                if int(line[2])<0: continue
                 chrom=line[0]
                 start=int(line[1])
                 tajimas_d=float(line[3])
                 if math.isnan(tajimas_d): tajimas_d=0
-                chromsomes[chrom].append(tajimas_d)
+                if start <= 0: start =1
+                chromsomes[chrom].append((start,tajimas_d))
         self.write_wig(chromsomes)
         
 
